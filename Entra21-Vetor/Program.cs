f503374892cbeb23ab872deace564264758cfa2c
GuilhermeDMueller <guilhermemuellerdzesigaleski@gmail.com>
2022-05-19 22:05:09 -0300

    Add project files.

---
+++ Entra21-Vetor/Program.cs
@@ -0,0 +1,16 @@
+﻿using Entra21_Vetor;
+
+Console.WriteLine(@" Ação
+1 | Atividade01");
+int numeroAcao = Convert.ToInt32(Console.ReadLine());
+
+if (numeroAcao == 1)
+{
+    VetorExercicio01 newExercicio = new VetorExercicio01();
+    newExercicio.Executar();
+}
+else if (numeroAcao == 2)
+{
+    VetorExercicio02 nesExercicio = new VetorExercicio02();
+    nesExercicio.Executar();
+}
---
+++ Entra21-Vetor/VetorExercicio01.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21_Vetor
+{
+    internal class VetorExercicio01
+    {
+        public void Executar()
+        {
+            string[] nomes = new string[3];
+            string[] sobreNomes = new string[3];
+
+            Console.WriteLine("Digite o nome: ");
+            nomes[0] = Console.ReadLine();
+
+            Console.WriteLine("Digite o sobrenome: ");
+            sobreNomes[0] = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome: ");
+            nomes[1] = Console.ReadLine();
+
+            Console.WriteLine("Digite o sobrenome: ");
+            sobreNomes[1] = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome: ");
+            nomes[2] = Console.ReadLine();
+
+            Console.WriteLine("Digite o sobrenome: ");
+            sobreNomes[2] = Console.ReadLine();
+
+            Console.WriteLine("Nome: " + nomes[0] + sobreNomes[0]);
+            Console.WriteLine("Nome: " + nomes[1] + sobreNomes[1]);
+            Console.WriteLine("Nome: " + nomes[2] + sobreNomes[2]);
+        }
+    }
+}
