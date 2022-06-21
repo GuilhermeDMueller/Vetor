717e03bc2c2cab85b1ba7975edaf30bac24ef3b1
Guilherme DM <guilhermemuellerdzesigaleski@gmail.com>
2022-06-20 22:16:13 -0300

    Continuaçao

---
+++ Entra21.ExemplosWindowsForms/Exemplo01/EnderecoServico.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExemplosWindowsForms.Exemplo01
+{
+    internal class EnderecoServico
+    {
+        private List<Endereco> enderecos;
+
+        // Construtor: Mais para Frente
+        public EnderecoServico()
+        {
+            enderecos = new List<Endereco>();
+        }
+        // Método Adicionar recebe como parametro o objeto do endereco completodo Form(Controler)
+        public void Adicionar(Endereco endereco)
+        {
+            enderecos.Add(endereco);
+
+            SalvarNoArquivo();
+        }
+
+        public void Editar(Endereco enderecoParaAlterar)
+        {
+            // Percorre lista de endereços afim de encontrar o enderço que deve ser alterado
+            for (int indice = 0; indice < enderecos.Count; indice++)
+            {
+                // Obtém o endereço da lista de endereços
+                var endereco = enderecos[indice];
+
+                // Verificar se o código do endereço percorrido é o mesmo do endereço que deve ser alterado
+                if(endereco.Codigo == enderecoParaAlterar.Codigo)
+                {
+                    // Atualizar campos do endereço encontrado
+                    endereco.EnderecoCompleto = enderecoParaAlterar.EnderecoCompleto;
+                    endereco.Cep = enderecoParaAlterar.Cep;
+                    endereco.Paciente = enderecoParaAlterar.Paciente;
+
+                    SalvarNoArquivo();
+
+                    return;
+                }
+            }
+        }
+        public void Apagar(int codigoParaApagar)
+        {
+            // Percorre lista de endereços afim de encontrar o enderço que deve ser removido
+            for (int indice = 0; indice < enderecos.Count; indice++)
+            {
+                // Obtém o endereço percorrido
+                var endereco = enderecos[indice];
+
+                if (endereco.Codigo == codigoParaApagar)
+                {
+                    // Remove o endereço encontrado da lista de endereços
+                    enderecos.Remove(endereco);
+
+                    SalvarNoArquivo();
+
+                    return;
+                }
+            }
+        }
+        // Método que permite listar todos os endereços
+        public List<Endereco> ObterTodos()
+        {
+            return enderecos;
+        }
+        public Endereco ObterPorCodigo(int codigo)
+        {
+            // Percorre lista de endereços afim de encontrar o enderço com o código
+            for (int indice = 0; indice < enderecos.Count; indice++)
+            {
+                // Obtém o endereço percorrido
+                var endereco = enderecos[indice];
+
+                if (endereco.Codigo == codigo)
+                {
+                    return endereco;
+                }
+            }
+            // Retorna null pois não encontrou o endereço com o código desejado
+            return null;
+        }
+
+        public void SalvarEmArquivo()
+        {
+            var enderecosEmJson = JsonConvert.SerializeObject(enderecos);
+            File.WriteAllText("endereco.json", enderecosEmJson);
+        }
+        public void LerArquivo()
+        {
+            // Verificar se o edereço não existe
+            if (File.Exists("enderecos.json") == false)
+                return;
+
+            // Ler arquivo JSON e converte para uma lista de objetos de endereços
+            var enderecoEmJson = File.ReadAllText("enderecos.json");
+            enderecos = JsonConvert.DeserializeObject<List<Endereco>>(enderecoEmJson);
+        }
+    }
+}
---
+++ Entra21.ExemplosWindowsForms/Exemplo01/EnderecosForm.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Entra21.ExemplosWindowsForms.Exemplo01
+{
+    partial class EnderecosForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelEnderecos = new System.Windows.Forms.Label();
+            this.labelCep = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.labelEnderecoCompleto = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelPaciente = new System.Windows.Forms.Label();
+            this.buttonEditar = new System.Windows.Forms.Button();
+            this.buttonApagar = new System.Windows.Forms.Button();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelEnderecos
+            // 
+            this.labelEnderecos.AutoSize = true;
+            this.labelEnderecos.Location = new System.Drawing.Point(26, 19);
+            this.labelEnderecos.Name = "labelEnderecos";
+            this.labelEnderecos.Size = new System.Drawing.Size(77, 20);
+            this.labelEnderecos.TabIndex = 0;
+            this.labelEnderecos.Text = "Endereços";
+            // 
+            // labelCep
+            // 
+            this.labelCep.AutoSize = true;
+            this.labelCep.Location = new System.Drawing.Point(613, 36);
+            this.labelCep.Name = "labelCep";
+            this.labelCep.Size = new System.Drawing.Size(34, 20);
+            this.labelCep.TabIndex = 1;
+            this.labelCep.Text = "CEP";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(613, 74);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(302, 27);
+            this.maskedTextBox1.TabIndex = 2;
+            // 
+            // labelEnderecoCompleto
+            // 
+            this.labelEnderecoCompleto.AutoSize = true;
+            this.labelEnderecoCompleto.Location = new System.Drawing.Point(613, 112);
+            this.labelEnderecoCompleto.Name = "labelEnderecoCompleto";
+            this.labelEnderecoCompleto.Size = new System.Drawing.Size(141, 20);
+            this.labelEnderecoCompleto.TabIndex = 3;
+            this.labelEnderecoCompleto.Text = "Endereço Completo";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(596, 475);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(613, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(474, 27);
+            this.textBox1.TabIndex = 6;
+            // 
+            // labelPaciente
+            // 
+            this.labelPaciente.AutoSize = true;
+            this.labelPaciente.Location = new System.Drawing.Point(613, 187);
+            this.labelPaciente.Name = "labelPaciente";
+            this.labelPaciente.Size = new System.Drawing.Size(64, 20);
+            this.labelPaciente.TabIndex = 7;
+            this.labelPaciente.Text = "Paciente";
+            // 
+            // buttonEditar
+            // 
+            this.buttonEditar.Location = new System.Drawing.Point(368, 12);
+            this.buttonEditar.Name = "buttonEditar";
+            this.buttonEditar.Size = new System.Drawing.Size(94, 29);
+            this.buttonEditar.TabIndex = 9;
+            this.buttonEditar.Text = "Editar";
+            this.buttonEditar.UseVisualStyleBackColor = true;
+            // 
+            // buttonApagar
+            // 
+            this.buttonApagar.Location = new System.Drawing.Point(493, 10);
+            this.buttonApagar.Name = "buttonApagar";
+            this.buttonApagar.Size = new System.Drawing.Size(94, 29);
+            this.buttonApagar.TabIndex = 10;
+            this.buttonApagar.Text = "Apagar";
+            this.buttonApagar.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Location = new System.Drawing.Point(897, 243);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(94, 29);
+            this.buttonCancelar.TabIndex = 11;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Location = new System.Drawing.Point(997, 243);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(94, 29);
+            this.buttonSalvar.TabIndex = 12;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(613, 210);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(474, 28);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // EnderecosForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1099, 544);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonSalvar);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.buttonApagar);
+            this.Controls.Add(this.buttonEditar);
+            this.Controls.Add(this.labelPaciente);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.labelEnderecoCompleto);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.labelCep);
+            this.Controls.Add(this.labelEnderecos);
+            this.Name = "EnderecosForm";
+            this.Text = "EnderecosForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label labelEnderecos;
+        private Label labelCep;
+        private MaskedTextBox maskedTextBox1;
+        private Label labelEnderecoCompleto;
+        private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private Label labelPaciente;
+        private Button buttonEditar;
+        private Button buttonApagar;
+        private Button buttonCancelar;
+        private Button buttonSalvar;
+        private ComboBox comboBox1;
+    }
+}
