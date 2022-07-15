cf63f28ac2dd55f67ed39e14e19597644ea26d84
Guilherme DMueller <guilhermemuellerdzesigaleski@gmail.com>
2022-07-15 15:51:12 -0300

    Tentativa de reslover problema do banco de dados. Falhou

---
+++ Listas/ListasExercicio/ExerciceList1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21_Listas.ListasExercicio
+{
+    internal class ExerciceList1
+    {
+        public void Executar()
+        {
+            List<string> filmes = new List<string>();
+            filmes.Add("Dragon Ball: Evollution");
+            filmes.Add("Titanic");
+            filmes.Add("Lanterna Verde");
+            filmes.Add("");
+            filmes.Add("Quarteto Fantástico");
+
+            filmes[0] = "Dragon Ball Super: Broly";
+            filmes[1] = "Titanic 2";
+
+            filmes.Remove("Lanterna Verde");
+
+            Console.Clear();
+            Console.WriteLine("[1]: " + filmes[0] + "\n[2]: " + filmes[1] + "\n[3]: " + filmes[2] + "\n[4]: " + filmes[3]);
+            if (filmes.Contains("Lanterna Verde") == true)
+                Console.WriteLine("O filme Lanterna Verde está cadastrado e pronto para assistir");
+            else
+                Console.WriteLine("O filme Lanterna Verde não está cadastrado");
+            
+            if (filmes.Contains("A Lagoa Azul") == true)
+                Console.WriteLine("O filme 'A Lagoa Azul' está cadastrado e pronto para assistir");
+            else
+                Console.WriteLine("O filme 'A Lagoa Azul' não está cadastrado");
+
+        }
+    }
+}
--- Listas/Program.cs
+++ Listas/Program.cs
@@ -1,10 +1,14 @@
 ﻿using Entra21___Listas.ListasExercicio;
+using Entra21_Listas.ListasExercicio;
 using Listas.ListasExercicio;
 
 Console.WriteLine(@"Informe a Ação Desejada: 
 1 | ListaExercicio01
 2 | ListaExercicio02
-3 | ListaExercicio03");
+3 | ListaExercicio03
+4 | ListaExercicio01-2
+5 | ListaExercicio02-2
+6 | ListaExercicio03-2");
 
 var acaoDesejada = Convert.ToInt32(Console.ReadLine());
 if (acaoDesejada == 1)
@@ -21,4 +25,9 @@ else if (acaoDesejada == 3)
 {
     ListaExercicio03 oldExercicie = new ListaExercicio03();
     oldExercicie.Executar();
+}
+else if (acaoDesejada == 4)
+{
+    ExerciceList1 dloExercice = new ExerciceList1();
+    dloExercice.Executar();
 }
